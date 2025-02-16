b5f0570259995b35cf09565fd36f122c1668846a
Zack Felcyn <zackfelcyn@icloud.com>
2025-02-15 16:48:41 -0800

    New Logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "../assets/Event-Hub.png"; // Import the logo from the assets folder
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>Event Creator</h1>
+      {/* Replace "Event Creator" with the logo */}
+      <img src={logo} alt="Event Hub Logo" className="logo" />
       <div className="nav-links">
         <a href="/">Home</a>
         <a href="/events">Events</a>
