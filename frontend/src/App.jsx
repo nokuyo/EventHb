1b29fcff74cd0cd0bbb991486cefc325dabed2e9
LuisZarate17 <146589262+LuisZarate17@users.noreply.github.com>
2025-02-16 03:45:55 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,6 @@ function App() {
                             </PrivateRoute>
                         }
                     />
-                    {/* New admin route */}
                     <Route 
                         path="/admin" 
                         element={
