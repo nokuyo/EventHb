b4f9dba13f4cb5278fd2a7a56be936390c382a55
zfelcyn <zachary.felcyn@wsu.edu>
2025-04-25 12:41:38 -0700

    Express

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['a7a2-52-119-109-1.ngrok-free.app']
-  }
-})
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    allowedHosts: ["a7a2-52-119-109-1.ngrok-free.app"],
+  },
+});
